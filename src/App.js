6a6404dd57c1ad861243686bd21966fd234c5f20
Kağan Değirmen <kagandegirmen@gmail.com>
2023-02-08 08:45:08 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  /*Data array in a separate js file
+  Title, location, Google Maps link, start date, end date, description, image URL
+  Use . map () and props
+  Styled & polished */
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
