a3d48f408cb9c47c72d150f80284568b10877945
Kağan Değirmen <kagandegirmen@gmail.com>
2023-02-12 22:14:01 +0300

    Card UI fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,21 +4,21 @@ import pin from "../images/pin.svg";
 export default function Card(props) {
   return (
     <div className="card">
-      <img className="card--img" src={props.item.imageUrl} />
-      <div>
-        <div className="card--div--location">
-          <img src={pin} width={28} height={28} />
-          <h3 className="card--h3--location">{props.item.location}</h3>
-          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-        </div>
+      <div className="card--container">
+        <img className="card--img" src={props.item.imageUrl} />
         <div>
-          <div style={{ display: "flex", gap: 20 }}>
+          <div className="card--div--location">
+            <img src={pin} width={28} height={28} />
+            <h3 className="card--h3--location">{props.item.location}</h3>
+            <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+          </div>
+          <div>
+            <h1>{props.item.title}</h1>
             <h3>
               {props.item.startDate} - {props.item.endDate}
             </h3>
+            <p>{props.item.description}</p>
           </div>
-          <h1>{props.item.title}</h1>
-          <p>{props.item.description}</p>
         </div>
       </div>
     </div>
